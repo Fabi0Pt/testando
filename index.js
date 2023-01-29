1f1ef2b4983812eaaddea3bfae266d6914565faa
fabi0_pt@hotmail.com <fabi0_pt@hotmail.com>
2023-01-29 00:05:28 -0300

    teste

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-import pkg from "whatsapp-web.js";
+import { Client } from "whatsapp-web.js";
 import qrcode from "qrcode-terminal";
 
-export const connectionChat = async () => {
-  const { Client } = pkg;
-  const client = new Client({
-    puppeteer: {
-      headless: true,
-      args: ["--no-sandbox"],
-      ignoreDefaultArgs: ["--disable-extensions"],
-    },
-  });
-};
+const client = new Client({
+  puppeteer: {
+    headless: true,
+    args: ["--no-sandbox"],
+    ignoreDefaultArgs: ["--disable-extensions"],
+  },
+});
 
 client.on("qr", (qr) => {
+  console.log("QR received", qr);
   qrcode.generate(qr, { small: true });
 });
 
